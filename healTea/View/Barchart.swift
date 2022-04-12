2387e9ba13298b68d0355cc5d86fe9086a2f278a
Jungin Yoo <frog7134@gmail.com>
2022-04-12 17:15:00 +0900

    Revert "Donationdetail까지 구엘 데이터는 사용하지 못한채 손수 적었습니다.."

--- healTea/View/Barchart.swift
+++ healTea/View/Barchart.swift
@@ -6,29 +6,12 @@
 //
 
 import SwiftUI
-// import SwiftUICharts
-
-/*
-struct Barchart: View {
-    
-    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
-
-    var body: some View {
-        Barchart()
-            .data(demoData)
-            .chartStyle(ChartStyle(backgroundColor: .white, foregroundColor: ColorManager.DonationBar_1)
-    }
-}
-*/
- 
 
 struct ColorManager {
     static let DonationBackground = Color("DonationBackground")
     static let DonationBar_1 = Color("DonationBar_1")
     static let DonationBar_2 = Color("DonationBar_2")
     static let DonationBar_3 = Color("DonationBar_3")
-    static let DonationFont = Color("DonationFont")
-    static let DonationList = Color("DonationList")
 }
 
 struct Barchart: View {
@@ -39,95 +22,66 @@ struct Barchart: View {
         ZStack {
             //ColorManager.BackgroundColor.ignoresSafeArea()  // Color만 하면 위와 아랫부분 베젤은 색이 없기 때문에 ignore로 채우기 가능
             VStack {
-        // Picker(selection: $selectedColor, label: Text("")) {
-        //     Text("Day").tag(0)  // tag를 추가하면 픽했을 때 표시가 됨
-        //     Text("Month").tag(1)
-        // }
-        // .pickerStyle(SegmentedPickerStyle())
-        // .padding(.horizontal, 24)
+                Picker(selection: $selectedColor, label: Text("")) {
+                    Text("Day").tag(0)  // tag를 추가하면 픽했을 때 표시가 됨
+                    Text("Month").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal, 24)
                 
                 HStack(alignment: .bottom) {
-                    
                     VStack {
-                        BarView_1(value: 60)
+                        RoundedRectangle(cornerRadius: 10).frame(width:30, height: 60)                            .foregroundColor(ColorManager.DonationBar_1)
                         Text("1월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                     VStack {
-                        BarView_1(value: 80)
+                        RoundedRectangle(cornerRadius: 10).frame(width:30, height: 100)                            .foregroundColor(ColorManager.DonationBar_1)
                         Text("2월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                     VStack {
-                        BarView_1(value: 70)
+                        RoundedRectangle(cornerRadius: 10).frame(width:30, height: 80)                            .foregroundColor(ColorManager.DonationBar_1)
                         Text("3월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                     VStack {
-                        BarView_1(value: 100)
+                        RoundedRectangle(cornerRadius: 10).frame(width:30, height: 90)                            .foregroundColor(ColorManager.DonationBar_1)
                         Text("4월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                     VStack {
-                        BarView_2(value: 120)
+                        RoundedRectangle(cornerRadius: 10).frame(width:30, height: 130)                            .foregroundColor(ColorManager.DonationBar_2)
                         Text("5월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                     VStack {
-                        BarView_3()
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width:30, height: 120)
+                            .foregroundColor(ColorManager.DonationBar_3)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 9)
+                                    .frame(width: 28, height: 118)
+                                    .foregroundColor(.white)
+                            )
+                        
                         Text("6월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                     VStack {
-                        BarView_3()
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width:30, height: 120)
+                            .foregroundColor(ColorManager.DonationBar_3)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 9)
+                                    .frame(width: 28, height: 118)
+                                    .foregroundColor(.white)
+                            )
                         Text("7월")
-                            .foregroundColor(ColorManager.DonationFont)
                     }
                 }
-                .padding(.horizontal, 60)
-                .animation(.spring(), value: selectedColor)
+                .padding(.top, 24)
             }
         }
-    }
-}
-
-struct BarView_1: View {
-    
-    var value: CGFloat
-    
-    var body: some View {
-        RoundedRectangle(cornerRadius: 10).frame(width:30, height: value)
-            .foregroundColor(ColorManager.DonationBar_1)
-        
-    }
-}
-
-struct BarView_2: View {
-    
-    var value: CGFloat
-    
-    var body: some View {
-        RoundedRectangle(cornerRadius: 10).frame(width:30, height: value)
-            .foregroundColor(ColorManager.DonationBar_2)
-        
-    }
-}
-
-struct BarView_3: View {
-    
-    var body: some View {
-        RoundedRectangle(cornerRadius: 10).frame(width:30, height: 100)
-            .foregroundColor(ColorManager.DonationBar_3)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10).frame(width:28, height: 98)
-                    .foregroundColor(.white)
-            )
     }
 }
 
 struct Barchart_Previews: PreviewProvider {
     static var previews: some View {
         Barchart()
-            .previewInterfaceOrientation(.portrait)
     }
 }
