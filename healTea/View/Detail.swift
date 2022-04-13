5ee93dc84337af568124c1bb4bc2493ddcd58c9c
terry-koo <xpflxhfl94@naver.com>
2022-04-13 18:59:53 +0900

    Add Mypage2

--- healTea/View/Detail.swift
+++ healTea/View/Detail.swift
@@ -17,51 +17,62 @@ struct Detail: View {
     }
     
     var body: some View {
-        VStack{
-            HStack{
-                VStack{
-                    Text("자료 개수\(teas.count)")
-                    Text(teas[0].name)
-                        .font(.title)
-                    Text(teas[0].en_name)
-                    Text("")
-                    Text("\(teas[0].name)의 효능")
-                }// 차이름, 효능 타이틀
-                VStack{
-                    Image(teas[0].picture_code)
-                        .resizable()
-                        .frame(width: 200 , height: 200)
-                } // 차사진
-            }
-            
-            HStack{
-                ForEach(teas[0].good_name, id: \.self){ t in
-                    Text(t)
-                }
-            } // 효능
-            
-            HStack{
-                Text("")
-                Text("테이스팅 노트")
-                    .font(.title)
-            } // 테이스팅 노트
-            HStack{
-                ForEach(teas[0].tasting_note, id: \.self){ t in
-                    Text(t)
-                }
-            } // 테이스팅 노트 이미지
+        ScrollView{
             VStack{
-                Text("이런사람들 한테 좋아요")
-                    .font(.title)
-                ForEach(teas[0].good_context, id: \.self){ t in
-                    Text(t)
+                HStack{
+                    VStack{
+//                        Text("자료 개수\(teas.count)")
+                        Text(teas[0].name)
+                            .font(.title)
+                        Text(teas[0].en_name)
+                        Text("")
+                        
+                        Text("\(teas[0].name)의 효능")
+                    }// 차이름, 효능 타이틀
+                    VStack{
+                        Image(teas[0].picture_code)
+                            .resizable()
+                            .frame(width: 200 , height: 200)
+                    } // 차사진
                 }
-                Text("이런사람들 한테 나빠요")
-                    .font(.title)
-                ForEach(teas[0].bad_context, id: \.self){ t in
-                    Text(t)
+                
+                HStack{
+                    ForEach(teas[0].good_name, id: \.self){ t in
+                        Text(t)
+                    }
+                } // 효능
+                
+                HStack{
+                    Text("")
+                    Text("테이스팅 노트")
+                        .font(.title)
+                } // 테이스팅 노트
+                HStack{
+                    ForEach(teas[0].tasting_note, id: \.self){ t in
+                        Text(t)
+                    }
+                } // 테이스팅 노트 이미지
+                VStack{
+                    Text("이런사람들 한테 좋아요")
+                        .font(.title)
+                    ForEach(teas[0].good_context, id: \.self){ t in
+                        Text(t)
+                    }
+                    Text("이런사람들 한테 나빠요")
+                        .font(.title)
+                    ForEach(teas[0].bad_context, id: \.self){ t in
+                        Text(t)
+                    }
                 }
             }
         }
   }
+}
+
+
+
+struct Detail_Previews: PreviewProvider {
+    static var previews: some View {
+        Detail(id: TeaData.sampleData[0].id)
+    }
 }
---
+++ healTea/View/Mypage2.swift
@@ -0,0 +1,135 @@
+//
+//  Mypage2.swift
+//  healTea
+//
+//  Created by Terry Koo on 2022/04/13.
+//
+
+import SwiftUI
+import Foundation
+
+struct Mypage2: View {
+    @State var cha: Bool = true
+    @State var modify: Bool = false
+    var body: some View {
+        NavigationView{
+            VStack{
+                HStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 40)
+                            .strokeBorder(Color("MyGreen"), lineWidth: 3)
+                            .frame(width: 140, height: 45)
+                        
+                        Button(action: {
+                            cha.toggle()
+                            modify.toggle()
+                        }, label: {
+                            Text("나의 케릭터")
+                        })
+                        
+                        // 나의 캐릭터
+                    }
+                    
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 40)
+                            .strokeBorder(Color("MyGreen"), lineWidth: 3)
+                            .frame(width: 140, height: 45)
+                        
+                        Button(action: {
+                            cha.toggle()
+                            modify.toggle()
+                        }, label: {
+                            Text("정보수정")
+                        })
+                        
+                        // 정보 수정
+                    }
+                    
+                        
+
+                        
+                }
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.white)
+                        .shadow(color: .gray, radius: 3, x: 4, y: 4)
+                        .padding(.all)
+                        .frame(height: 300)
+                    
+                    //바뀌는 화면
+                    // 엘리스 시작
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    // 엘리스 끝
+                    
+                }
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color("MyGreen"))
+                        .shadow(color: .gray, radius: 3, x: 4, y: 4)
+                        .padding([.leading, .bottom, .trailing])
+                        .frame(height: 300)
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text("티 선호도 조사결과")
+                                .font(.title)
+                                
+                            Text("산뜻한 카모마일")
+                            Text("")
+                            Text("김승훈님은 약간의 카페인이")
+                            Text("첨가된 음료가 어울려요")
+                            
+                            Spacer().frame(height:105)
+                            
+                            
+                        }
+                        .foregroundColor(Color.white)
+                     
+                        
+                        VStack{
+                            Image("tea_0")
+                                .resizable()
+                                .clipShape(Circle())
+                                .shadow(radius: 3)
+                                .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                                .frame(width: 100, height: 100)
+                            
+                            Spacer().frame(height:75)
+                            
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .strokeBorder(.white, lineWidth: 2)
+                                    .frame(width: 120, height: 45)
+                                
+                                
+                                
+                                NavigationLink(destination: Text("다시 조사하는 페이지")) {
+                                    Text("다시 조사하기").foregroundColor(.white)
+                                }
+                            }
+                        
+                        }
+                    }
+                }
+                Spacer().frame(height:150)
+            }
+        }
+        
+    }
+}
+
+struct Mypage2_Previews: PreviewProvider {
+    static var previews: some View {
+        Mypage2()
+    }
+}
