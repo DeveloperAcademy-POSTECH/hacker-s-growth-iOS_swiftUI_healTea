3a60905ccf3f60c53a61da4928953985a12e2ff7
terry-koo <xpflxhfl94@naver.com>
2022-04-14 15:49:56 +0900

    final reset

--- healTea/View/Detail.swift
+++ healTea/View/Detail.swift
@@ -8,66 +8,20 @@
 import Foundation
 import SwiftUI
 
-
-
 struct Detail: View {
+//    var id: UUID
     var name: String
     var teas: [TeaData]{
         TeaData.sampleData.filter{ (t: TeaData) -> Bool in
-            print(name)
             return self.name == t.name
-            
         }
     }
-   
+    
     var body: some View {
-
-        NavigationView{
-        
-        
-        
-        
-        
-        VStack{
-            HStack{
-                VStack{
-                    Text("자료 개수\(teas.count)")
-                    Text(teas[0].name)
-                        .font(.title)
-                    Text(teas[0].en_name)
-                    Text("")
-                    Text("\(teas[0].name)의 효능")
-                }// 차이름, 효능 타이틀
-                VStack{
-                    Image(teas[0].picture_code)
-                        .resizable()
-                        .frame(width: 200 , height: 200)
-                } // 차사진
-            }
-            
-            HStack{
-                ForEach(teas[0].good_name, id: \.self){ t in
-                    Text(t)
-                }
-            } // 효능
-            
-            HStack{
-                Text("")
-                Text("테이스팅 노트")
-                    .font(.title)
-            } // 테이스팅 노트
-            HStack{
-                ForEach(teas[0].tasting_note, id: \.self){ t in
-                    Text(t)
-                }
-            } // 테이스팅 노트 이미지
-
         ScrollView{
-
             VStack{
                 HStack{
                     VStack{
-                        
 //                        Text("자료 개수\(teas.count)")
                         Text(teas[0].name)
                             .font(.title)
@@ -75,9 +29,6 @@ struct Detail: View {
                         Text("")
                         
                         Text("\(teas[0].name)의 효능")
-                        Button(action: {
-                            print(teas[0].name)
-                        }, label: {Text("test")})
                     }// 차이름, 효능 타이틀
                     VStack{
                         Image(teas[0].picture_code)
@@ -121,10 +72,8 @@ struct Detail: View {
 
 
 
-//
-//struct Detail_Previews: PreviewProvider {
-//    static var previews: some View {
-//       // Detail(name: TeaData.sampleData[0].name)
-//    }
-//}
-
+struct Detail_Previews: PreviewProvider {
+    static var previews: some View {
+        Detail(name: TeaData.sampleData[0].name)
+    }
+}
